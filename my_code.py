ac608d23820fe0766ff413cf64abb88394d91aa7
Jenish Praveen Kumar G <137860558+JenishPraveenKumarG@users.noreply.github.com>
2023-09-12 23:49:10 +0530

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -1,11 +1,11 @@
 import random
 import string
-
+# function 
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
-
+# main class
 if __name__ == "__main__":
     password_length = int(input("Enter desired password length: "))
     password = generate_password(password_length)
